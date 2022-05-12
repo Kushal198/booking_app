e5c10e813af8c38e461cbaccb0c4c53eefb70129
kushal198 <kushalkc198@gmail.com>
2022-05-12 11:43:57 +0545

    initial

---
+++ server/index.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import authRoute from './api/routes/auth.js';
+import usersRoute from './api/routes/users.js';
+import hotelsRoute from './api/routes/hotels.js';
+import roomRoute from './api/routes/rooms.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try{
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongodb");
+  }catch(err){
+    throw err;
+  }
+}
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB connected");
+})
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+//middlewares
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/rooms', roomRoute);
+
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong"
+  return res.status(errorStatus).json({
+    success:false,
+    status:errorStatus,
+    message:errorMessage,
+    stack:err.stack
+  });
+})
+
+
+
+app.listen(8000, () => {
+  connect();
+  console.log("Connected to backend")
+})
